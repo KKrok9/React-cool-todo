9d82d46d661a68133f7816853d50f0d4e8375465
KKrok9@gmail.com <kacperkrok9@gmail.com>
2023-02-20 12:51:26 +0100

    debbuged version, have to add labels with some errors or smthin like that

--- src/components/SingleTodo/SingleTodo.js
+++ src/components/SingleTodo/SingleTodo.js
@@ -6,21 +6,25 @@ const SingleTodo = (props) => {
   const [todos, setTodos] = useState(
     JSON.parse(localStorage.getItem(props.currentUserTodosKey))
   );
-  
-  const removeItem = () =>{
-    setTodos(todos.splice(props.index,1));
+
+  const removeItem = () => {
+    setTodos(todos.splice(props.index, 1));
     localStorage.setItem(
       props.currentUserTodosKey.toString(),
       JSON.stringify([todos])
     );
     props.updateUpTodos(todos);
-  }
+  };
   return (
     <div>
-      <div className={styles["todo-row"]}>
-        <p className={styles["todo-row__item"]}>{props.todoValue}</p>
-        <button className={styles["todo-row__item"]} onClick={removeItem}>Mark as done</button>
-      </div>
+      {props.todoValue.length !== 0 && 
+        <div className={styles["todo-row"]}>
+          <p className={styles["todo-row__item"]}>{props.todoValue}</p>
+          <button className={styles["todo-row__item"]} onClick={removeItem}>
+            Mark as done
+          </button>
+        </div>
+      }
     </div>
   );
 };
